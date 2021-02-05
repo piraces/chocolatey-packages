99062f901c29245ddd060cd0dd3408ba8dda276c
piraces <raul.piraces@gmail.com>
2021-02-05 11:01:48 +0100

    Update Ticker package

--- packages/ticker/tools/chocolateyInstall.ps1
+++ packages/ticker/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 
 Get-ChocolateyWebFile -PackageName 'ticker' -FileFullPath "$toolsDir\ticker.tar.gz" `
--Url 'https://github.com/achannarasappa/ticker/releases/download/v2.1.0/ticker-2.1.0-windows-386.tar.gz' `
--Url64bit 'https://github.com/achannarasappa/ticker/releases/download/v2.1.0/ticker-2.1.0-windows-amd64.tar.gz'
+-Url 'https://github.com/achannarasappa/ticker/releases/download/v2.3.0/ticker-2.3.0-windows-386.tar.gz' `
+-Url64bit 'https://github.com/achannarasappa/ticker/releases/download/v2.3.0/ticker-2.3.0-windows-amd64.tar.gz'
 
 Get-ChocolateyUnzip -FileFullPath "$toolsDir\ticker.tar.gz" -Destination $toolsDir
 
