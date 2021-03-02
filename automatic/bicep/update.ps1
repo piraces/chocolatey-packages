38928c4a4eb6bd7283c7e9609f41e683af3c38be
piraces <raul.piraces@gmail.com>
2021-03-02 14:59:55 +0100

    [AU bicep ]

--- automatic/bicep/update.ps1
+++ automatic/bicep/update.ps1
@@ -8,7 +8,7 @@ function global:au_SearchReplace {
             "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
             "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
         }
-        ".tools\VERIFICATION.txt" = @{
+        "tools\VERIFICATION.txt" = @{
             "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
             "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
         }
