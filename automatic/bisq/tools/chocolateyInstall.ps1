8b70470bd8b0507c6205924ff3988b7d3a4c2a57
piraces <raul.piraces@gmail.com>
2021-03-22 20:56:28 +0100

    [AU bisq ]

---
+++ automatic/bisq/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'bisq'
+$fileType    = 'exe'
+$url64       = 'https://github.com/bisq-network/bisq/releases/download/v1.5.9/Bisq-64bit-1.5.9.exe'
+$checksum64  = '505dcdafc336daff73d0397de979df2c5567d03d6a7e4f37b8922c5819eb988a'
+$silentArgs  = '/VERYSILENT /NORESTART /SUPRESSMSGBOXES'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0, 3010, 1641)
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = $fileType
+  url64          = $url64
+  checksum64     = $checksum64
+  checksumType64 = $checksumType64
+  validExitCodes = $validExitCodes
+  silentArgs     = $silentArgs
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/bisq/update.ps1
@@ -0,0 +1,29 @@
+import-module au
+
+$releases = 'https://github.com/bisq-network/bisq/releases'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    # Bisq-64bit-1.5.9.exe
+    $re  = "Bisq-64bit-.+.exe"
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+    $version = $url -split '/' | select -Last 1 -Skip 1
+    $version = $version.replace("v","")
+    $url64 = 'https://github.com' + $url
+
+    $Latest = @{ URL64 = $url64; Version = $version }
+    return $Latest
+}
+
+update
