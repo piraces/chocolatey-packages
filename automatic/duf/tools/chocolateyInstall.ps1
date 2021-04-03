71a47d5decf4fe78dec87f97f637e344a199b10e
Chocolatey <chocolatey@realdimensions.net>
2021-04-03 00:05:55 +0000

    AU: 1 updated - duf [skip ci] https://gist.github.com/934662dc159cbb8aa7dade125b15588f

--- automatic/duf/tools/chocolateyInstall.ps1
+++ automatic/duf/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'duf'
-$url32       = 'https://github.com/muesli/duf/releases/download/v0.6.1/duf_0.6.1_Windows_i386.zip'
-$url64       = 'https://github.com/muesli/duf/releases/download/v0.6.1/duf_0.6.1_Windows_x86_64.zip'
-$checksum32  = '9888b0d806485d516ae04e1c68adcdfdef9d5b2d530339b97ce9d56b59276766'
-$checksum64  = '4a96f247405945ca3c45bb0853fd6af09dc5c0885e9a3116795cece50ef5fe26'
+$url32       = 'https://github.com/muesli/duf/releases/download/v0.6.2/duf_0.6.2_Windows_i386.zip'
+$url64       = 'https://github.com/muesli/duf/releases/download/v0.6.2/duf_0.6.2_Windows_x86_64.zip'
+$checksum32  = 'e4d4135f2d67b37b0dc63cb457f69036672307148f864d2d4c86ff881d2ea133'
+$checksum64  = '16ef7c5ae344f7fe747b0e5bd1fe4f7a9a7a6475bd0672e3ddce402469ed79b2'
 
 $packageArgs = @{
   packageName    = $packageName
