9717770d6a18adceff5a21e0fbcf038eddc78abd
piraces <raul.piraces@gmail.com>
2021-05-01 23:13:06 +0200

    [AU bisq ]

--- automatic/bisq/tools/chocolateyInstall.ps1
+++ automatic/bisq/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $packageName = 'bisq'
 $fileType    = 'exe'
 $url64       = 'https://github.com/bisq-network/bisq/releases/download/v1.6.3/Bisq-64bit-1.6.3.exe'
 $checksum64  = 'ce69e7069d554fb7fb2a6e5086d498bce5dfe366582b655d5f13573009892c95'
-$silentArgs  = '/VERYSILENT /NORESTART /SUPRESSMSGBOXES'
+$silentArgs  = '/quiet'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0, 3010, 1641)
 
