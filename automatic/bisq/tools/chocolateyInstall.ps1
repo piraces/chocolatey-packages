f1fc6969930459d82c750ee22f97e0e404070299
Chocolatey <chocolatey@realdimensions.net>
2021-05-01 16:06:34 +0000

    AU: 1 updated - bisq [skip ci] https://gist.github.com/934662dc159cbb8aa7dade125b15588f

--- automatic/bisq/tools/chocolateyInstall.ps1
+++ automatic/bisq/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'bisq'
 $fileType    = 'exe'
-$url64       = 'https://github.com/bisq-network/bisq/releases/download/v1.6.2/Bisq-64bit-1.6.2.exe'
-$checksum64  = '9c45e2a92e1b2087998feccfcdfddf7f6111c3f9dc41466024a53d0953fdd22a'
+$url64       = 'https://github.com/bisq-network/bisq/releases/download/v1.6.3/Bisq-64bit-1.6.3.exe'
+$checksum64  = 'ce69e7069d554fb7fb2a6e5086d498bce5dfe366582b655d5f13573009892c95'
 $silentArgs  = '/VERYSILENT /NORESTART /SUPRESSMSGBOXES'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0, 3010, 1641)
