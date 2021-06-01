8739fb5dc0e6a9c70221e2539181ca4c46a95456
Chocolatey <chocolatey@realdimensions.net>
2021-06-01 08:06:39 +0000

    AU: 1 updated - ticker [skip ci] https://gist.github.com/934662dc159cbb8aa7dade125b15588f

--- automatic/ticker/tools/chocolateyInstall.ps1
+++ automatic/ticker/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'ticker'
-$url32       = 'https://github.com/achannarasappa/ticker/releases/download/v4.1.1/ticker-4.1.1-windows-386.tar.gz'
-$url64       = 'https://github.com/achannarasappa/ticker/releases/download/v4.1.1/ticker-4.1.1-windows-amd64.tar.gz'
-$checksum32  = '4837dbb1eabe18eeca1946dcdd3921d187b01b155def3ecfac266ac79ec42e03'
-$checksum64  = 'eb3c562c6fa755a1b5d60d5d1c54d7df90c7dc09bd5b5d50d1a8c1cf41c328c5'
+$url32       = 'https://github.com/achannarasappa/ticker/releases/download/v4.2.0/ticker-4.2.0-windows-386.tar.gz'
+$url64       = 'https://github.com/achannarasappa/ticker/releases/download/v4.2.0/ticker-4.2.0-windows-amd64.tar.gz'
+$checksum32  = '3e633cb819f41b6a6ce5a0d5648e3a933d0f67f3cd8c6da250a0bb1da6de14a0'
+$checksum64  = '8f887e405d86fa67cc11b23ef2f10c5132ef4f8c797aa164f05398d7924096fa'
 
 $toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 
