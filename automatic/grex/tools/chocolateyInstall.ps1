c468c8a2375e76fc07a4f819f019dd02aa5f6ec5
Chocolatey <chocolatey@realdimensions.net>
2021-09-15 16:06:10 +0000

    AU: 1 updated - grex [skip ci] https://gist.github.com/934662dc159cbb8aa7dade125b15588f/27290aae2b6c7f0e36d83976111e2e93f188e9a9

--- automatic/grex/tools/chocolateyInstall.ps1
+++ automatic/grex/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'grex'
-$url32       = 'https://github.com/pemistahl/grex/releases/download/v1.2.0/grex-v1.2.0-x86_64-pc-windows-msvc.zip'
-$checksum32  = 'da9c50a4e19cbf7b1c4a001a9252c1a097b8eebbb9ec0bbf3f88bc79030e7d73'
+$url32       = 'https://github.com/pemistahl/grex/releases/download/v1.3.0/grex-v1.3.0-x86_64-pc-windows-msvc.zip'
+$checksum32  = 'e4399720cd8078a4c405b22220115f5aa6f549283d74e4a90448eacb73d9b648'
 
 $packageArgs = @{
   packageName    = $packageName
