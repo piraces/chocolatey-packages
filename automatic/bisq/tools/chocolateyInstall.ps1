fc8ccea2d2f441a976d8250152c05b3f8b3d63cb
Chocolatey <chocolatey@realdimensions.net>
2022-03-04 16:05:50 +0000

    AU: 1 updated - bisq [skip ci] https://gist.github.com/934662dc159cbb8aa7dade125b15588f/e8e5877026c25a1f82752bdb39581e636224f9a7

--- automatic/bisq/tools/chocolateyInstall.ps1
+++ automatic/bisq/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'bisq'
 $fileType    = 'exe'
-$url64       = 'https://github.com/bisq-network/bisq/releases/download/v1.8.2/Bisq-64bit-1.8.2.exe'
-$checksum64  = '54b04b73789e62990aab77126d5c73d6894a543c5ea711e30e618e7b7ba00f04'
+$url64       = 'https://github.com/bisq-network/bisq/releases/download/v1.8.3/Bisq-64bit-1.8.3.exe'
+$checksum64  = '3d5bc79050dd456ce63c0447ced66909f5451fd63bcc890715756fdde2005cc7'
 $silentArgs  = '/quiet'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0, 3010, 1641)
