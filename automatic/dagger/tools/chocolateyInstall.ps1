1b6b6c2492b554ce091c1217959a94be5ea5c814
piraces <raul.piraces@gmail.com>
2022-04-05 22:24:05 +0200

    Add dagger

---
+++ automatic/dagger/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'dagger'
+$url64       = ''
+$checksum64  = ''
+
+$packageArgs = @{
+  packageName    = $packageName
+  url            = $url64
+  checksum       = $checksum64
+  checksumType   = 'sha256'
+  unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
+}
+Install-ChocolateyZipPackage @packageArgs
