074b7d4f1186be5c4e63da5f355d770e4f9cd838
piraces <raul.piraces@gmail.com>
2022-04-05 22:54:53 +0200

    Fix dagger

--- automatic/dagger/tools/chocolateyInstall.ps1
+++ automatic/dagger/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'dagger'
-$url64       = ''
-$checksum64  = ''
+$url64       = 'https://github.com/dagger/dagger/releases/download/v0.2.5/dagger_v0.2.5_windows_amd64.zip'
+$checksum64  = '872abeb12125f45e59969b208716a95371ff48a2dd39508c39731bd5f5ab8306'
 
 $packageArgs = @{
   packageName    = $packageName
