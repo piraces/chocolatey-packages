cbffbc28c8fade3ddb6dbf870ca6609059f497dc
piraces <raul.piraces@gmail.com>
2022-05-16 16:16:04 +0200

    Update aztfy

--- automatic/aztfy/tools/chocolateyInstall.ps1
+++ automatic/aztfy/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'aztfy'
-$url32       = ''
-$url64       = ''
-$checksum32  = ''
-$checksum64  = ''
+$url32       = 'https://github.com/Azure/aztfy/releases/download/v0.3.0/aztfy_v0.3.0_windows_386.zip'
+$url64       = 'https://github.com/Azure/aztfy/releases/download/v0.3.0/aztfy_v0.3.0_windows_amd64.zip'
+$checksum32  = '2668ac94314c9c3591442e25ff789d066711522d20a5f35862f8d46b347c2f9a'
+$checksum64  = 'ca780d094cbe2d84cf5cdf7cb6cf263c34759445d05147095d3704b883873f3f'
 
 $packageArgs = @{
   packageName    = $packageName
