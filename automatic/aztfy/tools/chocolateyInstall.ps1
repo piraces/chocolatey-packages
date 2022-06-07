59e6c578483fbd5f6349040485ad3910cb734eb5
piraces <raul.piraces+github@gmail.com>
2022-06-07 02:40:12 +0000

    AU: 1 updated - aztfy [skip ci] https://gist.github.com/934662dc159cbb8aa7dade125b15588f/410208f4ce7331e0940b554dc80ecc1380f47501

--- automatic/aztfy/tools/chocolateyInstall.ps1
+++ automatic/aztfy/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'aztfy'
-$url32       = 'https://github.com/Azure/aztfy/releases/download/v0.4.0/aztfy_v0.4.0_windows_386.zip'
-$url64       = 'https://github.com/Azure/aztfy/releases/download/v0.4.0/aztfy_v0.4.0_windows_amd64.zip'
-$checksum32  = '1b06ad238d01ee9ae6ee9e31f24cab7777cc4ee625ddfab32e50d9729ec9d947'
-$checksum64  = '38e33db61a7add51161fe8ef3d76690d2aa52e8ccfc3a0ae6931915a2b509be1'
+$url32       = 'https://github.com/Azure/aztfy/releases/download/v0.5.0/aztfy_v0.5.0_windows_386.zip'
+$url64       = 'https://github.com/Azure/aztfy/releases/download/v0.5.0/aztfy_v0.5.0_windows_amd64.zip'
+$checksum32  = 'a86b2e2f091ba6272bb4d9fb2a6154a04f8d275d627ad9b7ff386201197364d8'
+$checksum64  = '23cac3c684c9797651f0b93061c3860e67a8e948e1788c45f458749e905c6e6e'
 
 $packageArgs = @{
   packageName    = $packageName
