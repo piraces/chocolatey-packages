8dcc9ed79e97354222de0e85a7539becc61519ac
piraces <raul.piraces+github@gmail.com>
2022-06-14 21:07:45 +0000

    AU: 2 updated - dagger flyctl [skip ci] https://gist.github.com/934662dc159cbb8aa7dade125b15588f/232d60666295932180a8d2873b453841fab32f71

--- automatic/flyctl/tools/chocolateyInstall.ps1
+++ automatic/flyctl/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'flyctl'
-$url64       = 'https://github.com/superfly/flyctl/releases/download/v0.0.334-pre-1/flyctl_0.0.334-pre-1_Windows_x86_64.zip'
-$checksum64  = 'd391968116397325d40469cadbb7d8411075148d01983fc9587a454b19ac62b4'
+$url64       = 'https://github.com/superfly/flyctl/releases/download/v0.0.334-pre-3/flyctl_0.0.334-pre-3_Windows_x86_64.zip'
+$checksum64  = 'de74a9b38fa4bbfb33f195db39e310c7fb4c43be6239b42d15109b5a15a5f25f'
 
 $packageArgs = @{
   packageName    = $packageName
