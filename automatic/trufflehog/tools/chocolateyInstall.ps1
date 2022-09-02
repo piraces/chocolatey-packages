24eb8d06edceedf6e7fbc22196d5f5044fb5cc04
piraces <raul.piraces+github@gmail.com>
2022-09-02 03:59:57 +0000

    AU: 1 updated - trufflehog [skip ci] https://gist.github.com/934662dc159cbb8aa7dade125b15588f/654a5cbebcf1b6110bfaf0e97c37b9ed7066254a

--- automatic/trufflehog/tools/chocolateyInstall.ps1
+++ automatic/trufflehog/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'trufflehog'
-$url64       = 'https://github.com/trufflesecurity/trufflehog/releases/download/v3.10.0/trufflehog_3.10.0_windows_amd64.tar.gz'
-$checksum64  = 'a36824572ef141fb85e45711167352aaad6239c0f64863444cd63040377febdb'
+$url64       = 'https://github.com/trufflesecurity/trufflehog/releases/download/v3.10.1/trufflehog_3.10.1_windows_amd64.tar.gz'
+$checksum64  = '1f87c58d648947c266f2b3e6dac554f9be6a687529b0161e3428f92dc37e1af4'
 
 $toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 
