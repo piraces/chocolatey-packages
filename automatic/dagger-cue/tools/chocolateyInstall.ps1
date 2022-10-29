09ea271f54b09f57b70e439d6ebf371c2de2c518
piraces <raul.piraces+github@gmail.com>
2022-10-29 10:50:04 +0000

    AU: 1 updated - dagger-cue [skip ci] https://gist.github.com/934662dc159cbb8aa7dade125b15588f/596f430e57d9b70ac8c7d2f0d07b2d7ab6ef04e1

--- automatic/dagger-cue/tools/chocolateyInstall.ps1
+++ automatic/dagger-cue/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'dagger-cue'
-$url64       = ''
-$checksum64  = ''
+$url64       = 'https://dagger-io.s3.amazonaws.com/dagger-cue/releases/0.2.232/dagger-cue_v0.2.232_windows_amd64.zip'
+$checksum64  = '658b84bf9101c827826359bc4e6f8d7e1c9ff144b3e0224460a52337ffb954a0'
 
 $packageArgs = @{
   packageName    = $packageName
