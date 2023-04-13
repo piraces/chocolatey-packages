7df75aba537b0bde4caddaec58f3d1a3cb2102bd
piraces <raul.piraces@gmail.com>
2023-04-13 23:53:54 +0200

    feat: upgrade packages manually

--- automatic/aztfexport/tools/chocolateyInstall.ps1
+++ automatic/aztfexport/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'aztfexport'
-$url32       = ''
-$url64       = ''
-$checksum32  = ''
-$checksum64  = ''
+$url32       = 'https://github.com/Azure/aztfexport/releases/download/v0.11.0/aztfexport_v0.11.0_windows_386.zip'
+$url64       = 'https://github.com/Azure/aztfexport/releases/download/v0.11.0/aztfexport_v0.11.0_windows_amd64.zip'
+$checksum32  = '912e4a33c1a8ae4859577131493061fe4e4bd44c2b1f776fb07c3d6be707b10a'
+$checksum64  = 'e18d807a23af3622d8d0f43b9a1ce59ac53955a28ddda1df1105e8038e7c5f1d'
 
 $packageArgs = @{
   packageName    = $packageName
--- automatic/gitleaks/tools/chocolateyInstall.ps1
+++ automatic/gitleaks/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'gitleaks'
-$url32       = 'https://github.com/zricethezav/gitleaks/releases/download/v8.16.0/gitleaks_8.16.0_windows_x32.zip'
-$url64       = 'https://github.com/zricethezav/gitleaks/releases/download/v8.16.0/gitleaks_8.16.0_windows_x64.zip'
-$checksum32  = '8aaf48f07735ee71ebc6d2b67350837cacbb700c86c8140ad5ab71dbfe28e3a8'
-$checksum64  = '8caff67902c47e2148db529d816d4ce09b7ec37f4cc891def77d0d2ed9f4b57d'
+$url32       = 'https://github.com/gitleaks/gitleaks/releases/download/v8.16.2/gitleaks_8.16.2_windows_x32.zip'
+$url64       = 'https://github.com/gitleaks/gitleaks/releases/download/v8.16.2/gitleaks_8.16.2_windows_x64.zip'
+$checksum32  = '1F2D85E9B008A571DD4EEEF0B67DCE30B6BAA98C459943CA29E0C84F855E5A96'
+$checksum64  = 'C87F4A1D57F04D13DE4B68262EA0E65C83C7E5ADF736B681D7BA3BA5BC44C874'
 
 $packageArgs = @{
   packageName    = $packageName
