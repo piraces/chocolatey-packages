5c2380e622c884f1acecef31a08102b73476797c
piraces <raul.piraces+github@gmail.com>
2023-04-26 20:26:15 +0000

    AU: 1 updated - wayback [skip ci] https://gist.github.com/piraces/934662dc159cbb8aa7dade125b15588f/9c87dd1314248b5ad7d3158040af853a0297518c

--- automatic/wayback/tools/chocolateyInstall.ps1
+++ automatic/wayback/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'wayback'
-$url32       = ''
-$url64       = ''
-$checksum32  = ''
-$checksum64  = ''
+$url32       = 'https://github.com/wabarc/wayback/releases/download/v0.19.1/wayback-windows-386-0.19.1.zip'
+$url64       = 'https://github.com/wabarc/wayback/releases/download/v0.19.1/wayback-windows-amd64-0.19.1.zip'
+$checksum32  = 'de3a120a90e9b5fad8eab7dc06f3e24f01038e9bde837df9871c687888583fde'
+$checksum64  = 'c03ceebc2bbaab17a12bc834e90e76ef3db615afffdb63b7451a701d60ace0f0'
 
 $unzipLocation = (Split-Path $MyInvocation.MyCommand.Definition)
 
