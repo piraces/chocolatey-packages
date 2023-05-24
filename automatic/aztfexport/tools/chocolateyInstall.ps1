0fd257a43bd25f91d3eb3aa98476bb2863134327
piraces <raul.piraces+github@gmail.com>
2023-05-24 08:09:11 +0000

    AU: 1 updated - aztfexport [skip ci] https://gist.github.com/piraces/934662dc159cbb8aa7dade125b15588f/c41320716214e6c6fca8bd682668190e5c6c5905

--- automatic/aztfexport/tools/chocolateyInstall.ps1
+++ automatic/aztfexport/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'aztfexport'
-$url32       = 'https://github.com/Azure/aztfexport/releases/download/v0.11.0/aztfexport_v0.11.0_windows_386.zip'
-$url64       = 'https://github.com/Azure/aztfexport/releases/download/v0.11.0/aztfexport_v0.11.0_windows_amd64.zip'
-$checksum32  = '912e4a33c1a8ae4859577131493061fe4e4bd44c2b1f776fb07c3d6be707b10a'
-$checksum64  = 'e18d807a23af3622d8d0f43b9a1ce59ac53955a28ddda1df1105e8038e7c5f1d'
+$url32       = 'https://github.com/Azure/aztfexport/releases/download/v0.12.0/aztfexport_v0.12.0_windows_386.zip'
+$url64       = 'https://github.com/Azure/aztfexport/releases/download/v0.12.0/aztfexport_v0.12.0_windows_amd64.zip'
+$checksum32  = '32096c10c75bc53e98e1aa19c8949f6cd7d1601003865cb5bbc018057430fb37'
+$checksum64  = 'f298636a852ca75e78edef7dbdefa2a916f1c3b320283f880c30ff7c584d6cc3'
 
 $packageArgs = @{
   packageName    = $packageName
