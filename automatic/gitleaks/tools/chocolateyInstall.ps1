152c6909afe2e41a0a4df7cb47e01d2232bdd10f
piraces <raul.piraces+github@gmail.com>
2023-06-06 19:10:38 +0000

    AU: 1 updated - gitleaks [skip ci] https://gist.github.com/piraces/934662dc159cbb8aa7dade125b15588f/6cb6d75d55679bb29adc1b8131296d18258964af

--- automatic/gitleaks/tools/chocolateyInstall.ps1
+++ automatic/gitleaks/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'gitleaks'
-$url32       = 'https://github.com/gitleaks/gitleaks/releases/download/v8.16.3/gitleaks_8.16.3_windows_x32.zip'
-$url64       = 'https://github.com/gitleaks/gitleaks/releases/download/v8.16.3/gitleaks_8.16.3_windows_x64.zip'
-$checksum32  = '7d847d03ad6be34c7f33e4ecb00fc2607e556f6b7f2a6f7b1c589ef6cf314dc1'
-$checksum64  = '7209433451424289201669f09a80ddd44f52bc8f7a5f02f85e579f83c0fed086'
+$url32       = 'https://github.com/gitleaks/gitleaks/releases/download/v8.16.4/gitleaks_8.16.4_windows_x32.zip'
+$url64       = 'https://github.com/gitleaks/gitleaks/releases/download/v8.16.4/gitleaks_8.16.4_windows_x64.zip'
+$checksum32  = 'fb2a95d4ce70bd5bd91c8fb4919aa884fd0665938675b64e8a1f748ac0c61d51'
+$checksum64  = '5a5ff00a72113525e8ce29b12c9d52548f72b0c6df5678ddde2b317e66ef6693'
 
 $packageArgs = @{
   packageName    = $packageName
