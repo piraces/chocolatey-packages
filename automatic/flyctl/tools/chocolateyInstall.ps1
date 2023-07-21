cba20e2a4de684fc7b481493466497f76f0a7633
piraces <raul.piraces+github@gmail.com>
2023-07-21 23:05:47 +0000

    AU: 1 updated - flyctl [skip ci] https://gist.github.com/piraces/934662dc159cbb8aa7dade125b15588f/8c0e055401c4f9e9cd5cdf982a2b0e0e7a006dc2

--- automatic/flyctl/tools/chocolateyInstall.ps1
+++ automatic/flyctl/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'flyctl'
-$url64       = 'https://github.com/superfly/flyctl/releases/download/v0.1.63/flyctl_0.1.63_Windows_x86_64.zip'
-$checksum64  = '228ca14071cc5cffa3503dddd5db88a1bd35c94bb0bf200430330d1d582d0d0e'
+$url64       = 'https://github.com/superfly/flyctl/releases/download/v0.1.64/flyctl_0.1.64_Windows_x86_64.zip'
+$checksum64  = '7b01b83d93d60879b72d8998f39c330d8ec420d3400ea16d8daff8fcc39b9361'
 
 $packageArgs = @{
   packageName    = $packageName
