4ba5ccf62a256875a450b016f9fb8d5af9cf5101
piraces <raul.piraces@gmail.com>
2023-08-05 15:53:05 +0200

    fix: flyctl to use original installation path

--- automatic/flyctl/tools/chocolateyInstall.ps1
+++ automatic/flyctl/tools/chocolateyInstall.ps1
@@ -1,5 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+$FlyInstall = $env:FLYCTL_INSTALL
+$BinDir = if ($FlyInstall) {
+  "$FlyInstall\bin"
+} else {
+  "$Home\.fly\bin"
+}
+
 $packageName = 'flyctl'
 $url64       = 'https://github.com/superfly/flyctl/releases/download/v0.1.71/flyctl_0.1.71_Windows_x86_64.zip'
 $checksum64  = 'c7e982983c7acb0b3f908d5bfb6a69f3b3af338547af48952297798f90159c13'
@@ -9,6 +16,7 @@ $packageArgs = @{
   url64Bit       = $url64
   checksum64     = $checksum64
   checksumType64 = 'sha256'
-  unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
+  unzipLocation  = Split-Path $BinDir
 }
 Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyPath -PathToInstall "$(Split-Path $BinDir)"
---
+++ automatic/flyctl/tools/chocolateyUninstall.ps1
@@ -0,0 +1,21 @@
+$ErrorActionPreference = 'Stop'
+
+$FlyInstall = $env:FLYCTL_INSTALL
+$BinDir = if ($FlyInstall) {
+  "$FlyInstall\bin"
+} else {
+  "$Home\.fly\bin"
+}
+
+$packageName = 'flyctl'
+$url64       = 'https://github.com/superfly/flyctl/releases/download/v0.1.69/flyctl_0.1.69_Windows_x86_64.zip'
+$fileName = Split-Path -Path $url64 -Leaf
+
+Uninstall-ChocolateyZipPackage $packageName $fileName
+Write-Output "Using Uninstall-ChocolateyPath-Custom function";
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$modulePath = Join-Path "${toolsDir}" 'Uninstall-ChocolateyPath-Custom.ps1'
+Import-Module "${modulePath}"
+
+Uninstall-ChocolateyPath-Custom "$(Split-Path $BinDir)"
