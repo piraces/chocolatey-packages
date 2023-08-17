4aebf7d8089d59b3f8dffce89b060ef9819c26e9
piraces <raul.piraces+github@gmail.com>
2023-08-17 19:05:41 +0000

    AU: 1 updated - dagger [skip ci] https://gist.github.com/piraces/934662dc159cbb8aa7dade125b15588f/8fc86f215627e132c89b2c3d42f280d93c3ebf35

--- automatic/dagger/tools/chocolateyInstall.ps1
+++ automatic/dagger/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'dagger'
-$url64       = 'https://dagger-io.s3.amazonaws.com/dagger/releases/0.8.3/dagger_v0.8.3_windows_amd64.zip'
-$checksum64  = '40c403f58f56d5b73a8ba2c15afe391d029b4ffb83ddfaf5be653d5c6e34d1c0'
+$url64       = 'https://dagger-io.s3.amazonaws.com/dagger/releases/0.8.4/dagger_v0.8.4_windows_amd64.zip'
+$checksum64  = 'c2526472cf4573e06b33df6e17dd008c541f9f22d925565f1694a8acacaf17c3'
 
 $packageArgs = @{
   packageName    = $packageName
