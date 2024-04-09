387c6fdecabdbeda040cbf085654d2bf9292a1b3
piraces <raul.piraces+github@gmail.com>
2024-04-09 20:11:58 +0000

    AU: 1 updated - bisq [skip ci] https://gist.github.com/piraces/934662dc159cbb8aa7dade125b15588f/e1abf8ad59fe13ed345ac0b2ebf6a9ebc75738ce

--- automatic/bisq/tools/chocolateyInstall.ps1
+++ automatic/bisq/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'bisq'
 $fileType    = 'exe'
-$url64       = 'https://github.com/bisq-network/bisq/releases/download/v1.9.14/Bisq-64bit-1.9.14.exe'
-$checksum64  = '2b7e5c57d703e96494b0ae47c3a0beeaef62b75523ad54418985987fc75c373a'
+$url64       = 'https://github.com/bisq-network/bisq/releases/download/v1.9.15/Bisq-64bit-1.9.15.exe'
+$checksum64  = '120a324f922a1a1dcaab386fb740d118c1c25b7cae0c356c1e2be6ccffa82171'
 $silentArgs  = '/quiet'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0, 3010, 1641)
