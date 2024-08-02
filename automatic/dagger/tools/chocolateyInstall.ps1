c950f46b021b77fc7d8f5fb3133e043f0320b914
piraces <raul.piraces+github@gmail.com>
2024-08-02 12:09:42 +0000

    AU: 2 updated - dagger trufflehog [skip ci] https://gist.github.com/piraces/934662dc159cbb8aa7dade125b15588f/d7de0365b56060cae50f80f4b3144303d8fda068

--- automatic/dagger/tools/chocolateyInstall.ps1
+++ automatic/dagger/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'dagger'
-$url64       = 'https://dl.dagger.io/dagger/releases/0.12.3/dagger_v0.12.3_windows_amd64.zip'
-$checksum64  = 'e1b79deb93dbe6d01550fbda8cedc195cfa4e872a20d5062895aff5fd14075c0'
+$url64       = 'https://dl.dagger.io/dagger/releases/0.12.4/dagger_v0.12.4_windows_amd64.zip'
+$checksum64  = '61fa1588a67a4a60637a02cdf2c7e92ce6e8e1c674fb3d88f728ed1f616c118b'
 
 $packageArgs = @{
   packageName    = $packageName
--- automatic/trufflehog/tools/chocolateyInstall.ps1
+++ automatic/trufflehog/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'trufflehog'
-$url64       = 'https://github.com/trufflesecurity/trufflehog/releases/download/v3.80.5/trufflehog_3.80.5_windows_amd64.tar.gz'
-$checksum64  = '99b469acca9d7479c8ad9d9176c17fd36a7b20023c8a3a25e3de220b42a9a860'
+$url64       = 'https://github.com/trufflesecurity/trufflehog/releases/download/v3.80.6/trufflehog_3.80.6_windows_amd64.tar.gz'
+$checksum64  = 'afe0e08bc65fe79548ea560c7e502a4239cfba2484d4ab71d00f82d55885853c'
 
 $toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 
