3cafda3af053e0184cd2a4ec8a259a91a45521d0
piraces <raul.piraces+github@gmail.com>
2024-10-07 20:06:55 +0000

    AU: 1 updated - flyctl [skip ci] https://gist.github.com/piraces/934662dc159cbb8aa7dade125b15588f/ce840ecc2120ecb04dea6731ecaa2ce4d8c2cf38

--- automatic/flyctl/tools/chocolateyUninstall.ps1
+++ automatic/flyctl/tools/chocolateyUninstall.ps1
@@ -8,7 +8,7 @@ $BinDir = if ($FlyInstall) {
 }
 
 $packageName = 'flyctl'
-$url64       = 'https://github.com/superfly/flyctl/releases/download/v0.3.15/flyctl_0.3.15_Windows_x86_64.zip'
+$url64       = 'https://github.com/superfly/flyctl/releases/download/v0.3.16/flyctl_0.3.16_Windows_x86_64.zip'
 $fileName = Split-Path -Path $url64 -Leaf
 
 Uninstall-ChocolateyZipPackage $packageName $fileName
