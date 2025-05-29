d14e4ef3eed536a1879d2e3b2b14c4f6c48a0ebd
piraces <raul.piraces+github@gmail.com>
2025-05-29 03:26:39 +0000

    AU: 1 updated - ticker [skip ci] https://gist.github.com/piraces/934662dc159cbb8aa7dade125b15588f/f949c74514de448ae7ac1e4a5f9c1fa68cd0b8c4

--- automatic/ticker/tools/chocolateyInstall.ps1
+++ automatic/ticker/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'ticker'
-$url32       = 'https://github.com/achannarasappa/ticker/releases/download/v5.0.4/ticker-5.0.4-windows-386.tar.gz'
-$url64       = 'https://github.com/achannarasappa/ticker/releases/download/v5.0.4/ticker-5.0.4-windows-amd64.tar.gz'
-$checksum32  = '03b4f21d491b0d35164ec12781b1c35fc9b25b24f08c51ab02fbe9c6be290bb5'
-$checksum64  = '9e41a4ec8b10974a43c84a67d1a5956f33a4e3bb95212d5369485e3cb8bc86dc'
+$url32       = 'https://github.com/achannarasappa/ticker/releases/download/v5.0.5/ticker-5.0.5-windows-386.tar.gz'
+$url64       = 'https://github.com/achannarasappa/ticker/releases/download/v5.0.5/ticker-5.0.5-windows-amd64.tar.gz'
+$checksum32  = 'c3ba5de89e3090043d4d4380ee3fc576b1ca5729915db7709d3d16ecb698ff32'
+$checksum64  = '93ca7b10adb7c4e8d3ecb97a9319c895ba26414f1280361ba8fbf418f209fb93'
 
 $toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 
