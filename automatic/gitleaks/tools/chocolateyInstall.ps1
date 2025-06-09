3b610f7ffc24da62b5018e52a6b8f1f69c4690a5
piraces <raul.piraces+github@gmail.com>
2025-06-09 01:35:24 +0000

    AU: 2 updated - gitleaks goreleaser [skip ci] https://gist.github.com/piraces/934662dc159cbb8aa7dade125b15588f/3a6f334479ea82dccd60a0b86d630ccc4addd966

--- automatic/gitleaks/tools/chocolateyInstall.ps1
+++ automatic/gitleaks/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'gitleaks'
-$url32       = 'https://github.com/gitleaks/gitleaks/releases/download/v8.27.1/gitleaks_8.27.1_windows_x32.zip'
-$url64       = 'https://github.com/gitleaks/gitleaks/releases/download/v8.27.1/gitleaks_8.27.1_windows_x64.zip'
-$checksum32  = '5b0485b8617a7d8eef7736aeb4b0dad3033b9df45dbb70f708537a039836f06b'
-$checksum64  = '7a6f3aa2fa533e4511aa0555b6794558f8024e432c64b047b01f17558722bbd4'
+$url32       = 'https://github.com/gitleaks/gitleaks/releases/download/v8.27.2/gitleaks_8.27.2_windows_x32.zip'
+$url64       = 'https://github.com/gitleaks/gitleaks/releases/download/v8.27.2/gitleaks_8.27.2_windows_x64.zip'
+$checksum32  = 'f65d7bfdc0aa5e173dda4d35ffde17e4772854f5b933eb14fb57ca33d0c73913'
+$checksum64  = '3c6a58efa70e991d7816a8bd87d1db797818017fcb67cbf1861394b404a70a42'
 
 $packageArgs = @{
   packageName    = $packageName
--- automatic/goreleaser/tools/chocolateyInstall.ps1
+++ automatic/goreleaser/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'goreleaser'
-$url32       = 'https://github.com/goreleaser/goreleaser/releases/download/v2.10.1/goreleaser_Windows_i386.zip'
-$url64       = 'https://github.com/goreleaser/goreleaser/releases/download/v2.10.1/goreleaser_Windows_x86_64.zip'
-$checksum32  = '55ebd0897c73d56231fec8aa98229cf5c10e8b1b1417850d36f4ee5fd44870c3'
-$checksum64  = '810ed3a0b2ebb4df4fc1fd167feb4ac8b326e0a2d6e92c46b19d260362a8d60d'
+$url32       = 'https://github.com/goreleaser/goreleaser/releases/download/v2.10.2/goreleaser_Windows_i386.zip'
+$url64       = 'https://github.com/goreleaser/goreleaser/releases/download/v2.10.2/goreleaser_Windows_x86_64.zip'
+$checksum32  = '8e4b68aee37bf7c88a7dcbd2bcbdbb2e3fc200f7e18ba1060157d13ed2d95f1d'
+$checksum64  = 'f36077748b3e4cd466d407685cee9c105890203ad7e4efe9d89c99ebeb8f8eea'
 
 $packageArgs = @{
   packageName    = $packageName
