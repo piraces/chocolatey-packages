6923477b61c5fc59fcf9cc240d3bb898894bce16
piraces <raul.piraces+github@gmail.com>
2025-08-11 20:08:07 +0000

    AU: 1 updated - flyctl [skip ci] https://gist.github.com/piraces/934662dc159cbb8aa7dade125b15588f/40b2574b71be5693e7dc3a1de71fc17a1f991a58

--- automatic/flyctl/tools/chocolateyInstall.ps1
+++ automatic/flyctl/tools/chocolateyInstall.ps1
@@ -8,8 +8,8 @@ $BinDir = if ($FlyInstall) {
 }
 
 $packageName = 'flyctl'
-$url64       = 'https://github.com/superfly/flyctl/releases/download/v0.3.168/flyctl_0.3.168_Windows_x86_64.zip'
-$checksum64  = 'f80d1a6baeaca6555c7ae2c6edb03291fc5e8789178595ac6cda7a33ec7a1268'
+$url64       = 'https://github.com/superfly/flyctl/releases/download/v0.3.169/flyctl_0.3.169_Windows_x86_64.zip'
+$checksum64  = 'c2123485d532e1738a887064285e0b08b3e9a366fdf11a1d58186973c9cc5880'
 
 $packageArgs = @{
   packageName    = $packageName
--- automatic/flyctl/tools/chocolateyUninstall.ps1
+++ automatic/flyctl/tools/chocolateyUninstall.ps1
@@ -8,7 +8,7 @@ $BinDir = if ($FlyInstall) {
 }
 
 $packageName = 'flyctl'
-$url64       = 'https://github.com/superfly/flyctl/releases/download/v0.3.168/flyctl_0.3.168_Windows_x86_64.zip'
+$url64       = 'https://github.com/superfly/flyctl/releases/download/v0.3.169/flyctl_0.3.169_Windows_x86_64.zip'
 $fileName = Split-Path -Path $url64 -Leaf
 
 Uninstall-ChocolateyZipPackage $packageName $fileName
