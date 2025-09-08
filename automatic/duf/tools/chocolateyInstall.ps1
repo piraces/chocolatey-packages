367f2943e9f9fcb453910a04cdb4d85196b5ffc2
piraces <raul.piraces+github@gmail.com>
2025-09-08 21:06:29 +0000

    AU: 1 updated - duf [skip ci] https://gist.github.com/piraces/934662dc159cbb8aa7dade125b15588f/c53f99b5929d481702dc3a3784ecbca6ad35c623

--- automatic/duf/tools/chocolateyInstall.ps1
+++ automatic/duf/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'duf'
-$url32       = 'https://github.com/muesli/duf/releases/download/v0.8.1/duf_0.8.1_Windows_i386.zip'
-$url64       = 'https://github.com/muesli/duf/releases/download/v0.8.1/duf_0.8.1_Windows_x86_64.zip'
-$checksum32  = '1d8829b7f93dfe532bf29015b8826d6d384d6f25c751e9118a12b9ff63ae881a'
-$checksum64  = '61cb50b541b30b6b66f20453d8795e6395454224e1609fa8454682fcf8931926'
+$url32       = 'https://github.com/muesli/duf/releases/download/v0.9.1/duf_0.9.1_windows_i386.zip'
+$url64       = 'https://github.com/muesli/duf/releases/download/v0.9.1/duf_0.9.1_windows_x86_64.zip'
+$checksum32  = '716138691fb2ed2f73e3061a43f1d1e82f47057e192f80d48e82d4eb07a17e55'
+$checksum64  = '503934be81f847d9ddb1b739834217480633435ad16515dd199e372c0b2e1afc'
 
 $packageArgs = @{
   packageName    = $packageName
