f33a0d9c26ff72e3fc50508db29b7e967e984309
piraces <raul.piraces+github@gmail.com>
2025-10-10 14:08:31 +0000

    AU: 1 updated - cue-cli [skip ci] https://gist.github.com/piraces/934662dc159cbb8aa7dade125b15588f/6910398eba2ed7c450da42f61a017fb273dfbe2a

--- automatic/cue-cli/tools/chocolateyInstall.ps1
+++ automatic/cue-cli/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'cue-cli'
-$url64       = 'https://github.com/cue-lang/cue/releases/download/v0.14.1/cue_v0.14.1_windows_amd64.zip'
-$checksum64  = 'b2289feb775db9d10de6c039ffeccc8afb7a7fe0c656af891f8fce7f38aa1d8a'
+$url64       = 'https://github.com/cue-lang/cue/releases/download/v0.14.2/cue_v0.14.2_windows_amd64.zip'
+$checksum64  = '0a1c6a050494d2d8d671e15967d2d2ab2bd37dce48e193e59728242797c86bb0'
 
 $packageArgs = @{
   packageName    = $packageName
