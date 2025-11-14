f43bf320bc49493a5ac11211004b61fdbd69b6d4
piraces <raul.piraces+github@gmail.com>
2025-11-14 13:14:52 +0000

    AU: 1 updated - grex [skip ci] https://gist.github.com/piraces/934662dc159cbb8aa7dade125b15588f/a2a193aba9c9a03b8ed66de8cbd3d3756504a992

--- automatic/grex/tools/chocolateyInstall.ps1
+++ automatic/grex/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'grex'
-$url32       = 'https://github.com/pemistahl/grex/releases/download/v1.4.5/grex-v1.4.5-x86_64-pc-windows-msvc.zip'
-$checksum32  = 'ffd6e41a78cf97fd5442b16a504102c229ce78e8a07b0fb89ba1e39ac3ff469e'
+$url32       = 'https://github.com/pemistahl/grex/releases/download/v1.4.6/grex-v1.4.6-x86_64-pc-windows-msvc.zip'
+$checksum32  = '7691dbb8e46339a15d8362b79b2456f508266cf00a55fe685ed69a6259577485'
 
 $packageArgs = @{
   packageName    = $packageName
