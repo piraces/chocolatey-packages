c988d04b4e57dbc6a86ed2778eac5ebadbb016f6
piraces <raul.piraces+github@gmail.com>
2026-02-25 17:26:33 +0000

    AU: 1 updated - goreleaser [skip ci] https://gist.github.com/piraces/934662dc159cbb8aa7dade125b15588f/4322c1ecaeee9dae41704ac8d65336a09908cd61

--- automatic/goreleaser/tools/chocolateyInstall.ps1
+++ automatic/goreleaser/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'goreleaser'
-$url32       = 'https://github.com/goreleaser/goreleaser/releases/download/v2.14.0/goreleaser_Windows_i386.zip'
-$url64       = 'https://github.com/goreleaser/goreleaser/releases/download/v2.14.0/goreleaser_Windows_x86_64.zip'
-$checksum32  = '7e1ed5e68688dd2e588219901c44345db743e00db5639c8cd829908f3b705874'
-$checksum64  = '2cf253fa3a2b89fd2265032a1cd50cfbd7a6ae09223b909cbc21314454e14942'
+$url32       = 'https://github.com/goreleaser/goreleaser/releases/download/v2.14.1/goreleaser_Windows_i386.zip'
+$url64       = 'https://github.com/goreleaser/goreleaser/releases/download/v2.14.1/goreleaser_Windows_x86_64.zip'
+$checksum32  = 'e33377e8b06c51c0a9e6adb01192a73a10ce4dc428ca46e3727d23d86916fb39'
+$checksum64  = 'd7a3d8ba795e97ab8c4f8003630d300da164adf21fde5a4049440c20f15c3137'
 
 $packageArgs = @{
   packageName    = $packageName
